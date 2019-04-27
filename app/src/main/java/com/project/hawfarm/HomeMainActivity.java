0ed7a8fb654c505cf42c6b073341f8e9a67c1ef0
meetmk8985 <meetkakadiya710@gmail.com>
2019-04-27 14:07:12 +0530

    avtivity home main in add bar and view pager added

--- app/src/main/java/com/project/hawfarm/HomeMainActivity.java
+++ app/src/main/java/com/project/hawfarm/HomeMainActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,12 @@ public class HomeMainActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mainView = inflater.inflate(R.layout.activity_home_main, container, false);
 
+        ViewPager mViewPager = (ViewPager) mainView.findViewById(R.id.viewPage);
+        ImageAdapter adapterView = new ImageAdapter(getActivity().getApplicationContext());
+        mViewPager.setAdapter(adapterView);
+
         return mainView;
+
     }
 }
+
