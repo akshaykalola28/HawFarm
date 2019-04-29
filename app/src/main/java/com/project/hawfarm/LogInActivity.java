a3af19932770da028863a0250453e5df329b80b9
Searchati nakho <raxgajjar@gmail.com>
2019-04-29 13:38:37 +0530

    order details page added and login activity java file editing remaining

--- app/src/main/java/com/project/hawfarm/LogInActivity.java
+++ app/src/main/java/com/project/hawfarm/LogInActivity.java
@@ -6,23 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity {
+    private static final String TAG = "LogInActivity";
+    private static final int REQUEST_SIGNUP = 0;
 
-    TextView signUp;
-    Button logInButton;
-    TextView forgotPass;
+    EditText _emailText;
+    EditText _passwordText;
+    Button _loginButton;
+    TextView _signupLink;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        signUp = findViewById(R.id.link_signup);
-        logInButton = findViewById(R.id.btn_login);
-        forgotPass = findViewById(R.id.forgot_pass);
-
-        signUp.setOnClickListener(new View.OnClickListener() {
+        _signupLink = findViewById(R.id.link_signup);
+        _signupLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
@@ -30,18 +32,46 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
 
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LogInActivity.this, HomeActivity.class));
-            }
-        });
 
-        forgotPass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LogInActivity.this, ForgotPassword.class));
-            }
-        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
     }
 }
