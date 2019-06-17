47bee768e314d7ce4f7f807f3ebf15f4bb1aaf60
meetmk8985 <meetkakadiya710@gmail.com>
2019-06-17 12:28:16 +0530

    login activity cradencial set

--- app/src/main/java/com/project/hawfarm/LogInActivity.java
+++ app/src/main/java/com/project/hawfarm/LogInActivity.java
@@ -4,14 +4,36 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LogInActivity extends AppCompatActivity {
     private static final String TAG = "LogInActivity";
 
     TextView forgotPasswordField, signUpField;
+    EditText emailField, passField;
+    String email, pass;
+    Button bttn_login;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +52,20 @@ public class LogInActivity extends AppCompatActivity {
         }
         //End of Select Language activity
 
+        emailField = findViewById(R.id.input_email);
+        passField = findViewById(R.id.input_password);
+
+        bttn_login = (Button) findViewById(R.id.btn_login);
+        bttn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getValidData()) {
+                    Log.d(TAG, "Button Clicked!!");
+                    userLogin();
+                }
+            }
+        });
+
         forgotPasswordField = findViewById(R.id.forgot_pass);
         forgotPasswordField.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,4 +82,68 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean getValidData() {
+        boolean valid = false;
+        email = emailField.getText().toString().trim();
+        pass = passField.getText().toString().trim();
+
+
+        if (email.isEmpty()) {
+            emailField.setError("Enter E-mail");
+            emailField.requestFocus();
+        } else if (pass.isEmpty()) {
+            passField.setError("Enter Password");
+            passField.requestFocus();
+        } else {
+            valid = true;
+        }
+        return valid;
+    }
+
+    private void userLogin() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ServerData.LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.d(TAG, "in Responce");
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("responseSuccess");
+                            if (success.equals("true")) {
+                                String data = jsonObject.getString("data");
+                                Log.d(TAG, "data: " + data);
+                                Intent intent;
+                                intent = new Intent(LogInActivity.this, HomeActivity.class);
+                                intent.putExtra("userData", data);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                String data = jsonObject.getString("data");
+                                Snackbar.make(getWindow().getDecorView().findViewById(android.R.id.content),
+                                        data, Snackbar.LENGTH_INDEFINITE).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LogInActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", pass);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(LogInActivity.this);
+        requestQueue.add(stringRequest);
+    }
+
 }
--- app/src/main/java/com/project/hawfarm/ServerData.java
+++ app/src/main/java/com/project/hawfarm/ServerData.java
@@ -3,4 +3,5 @@ package com.project.hawfarm;
 public class ServerData {
 
     public static final String REGISTER_URL = "http://hawfarm.openode.io/user/register";
+    public static final String LOGIN_URL = "http://hawfarm.openode.io/user/login";
 }
