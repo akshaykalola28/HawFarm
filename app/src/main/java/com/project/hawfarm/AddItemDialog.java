c4f237405a9b464c2d581aafffba990c29916dd7
rxstr <raxgajjar@gmail.com>
2019-07-23 11:00:03 +0530

    add item dialog completed

---
+++ app/src/main/java/com/project/hawfarm/AddItemDialog.java
@@ -0,0 +1,27 @@
+package com.project.hawfarm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddItemDialog extends DialogFragment {
+
+    View mainView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mainView = inflater.inflate(R.layout.custompopup, container, false);
+
+        return mainView;
+    }
+}
--- app/src/main/java/com/project/hawfarm/adapter/ItemAdapter.java
+++ app/src/main/java/com/project/hawfarm/adapter/ItemAdapter.java
@@ -2,13 +2,17 @@ package com.project.hawfarm.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.project.hawfarm.AddItemDialog;
 import com.project.hawfarm.R;
 
 import org.json.JSONObject;
@@ -27,11 +31,9 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         this.fragment = fragment;
     }
 
-    @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_cardview, viewGroup, false);
-
         return new ItemViewHolder(view);
     }
 
@@ -45,6 +47,15 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        itemViewHolder.itemAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = fragment.getFragmentManager().beginTransaction().addToBackStack(null);
+                DialogFragment dialog = new AddItemDialog();
+                dialog.show(fragmentTransaction, "addItemDialog");
+            }
+        });
     }
 
     @Override
@@ -55,6 +66,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
     public class ItemViewHolder extends RecyclerView.ViewHolder {
 
         TextView itemName, price, description;
+        Button itemAddButton;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -62,7 +74,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
             itemName = itemView.findViewById(R.id.item_name);
             price = itemView.findViewById(R.id.item_price);
             description = itemView.findViewById(R.id.item_description);
+            itemAddButton = itemView.findViewById(R.id.item_add_button);
         }
-
     }
 }
