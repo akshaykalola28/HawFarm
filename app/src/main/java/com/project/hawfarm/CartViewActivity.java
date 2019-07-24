d6e3db47098b513f018d03e625c55b9ba74dab1e
meetmk8985 <meetkakadiya710@gmail.com>
2019-07-24 10:41:26 +0530

    Activity Cart View Added

---
+++ app/src/main/java/com/project/hawfarm/CartViewActivity.java
@@ -0,0 +1,14 @@
+package com.project.hawfarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_view);
+    }
+
+}
