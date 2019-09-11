f8c5b04f2e4b05a505a8d6dc9e92d073c47085c2
abc <harshang.008@gmail.com>
2019-09-11 02:21:03 -0400

    add animation in relativeLayout at home page

--- app/src/main/java/com/project/hawfarm/adapter/HawkerAdapter.java
+++ app/src/main/java/com/project/hawfarm/adapter/HawkerAdapter.java
@@ -1,6 +1,8 @@
 package com.project.hawfarm.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -8,7 +10,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.project.hawfarm.ItemListFragment;
@@ -20,11 +25,14 @@ import org.json.JSONObject;
 
 import java.util.List;
 
+import static com.project.hawfarm.R.*;
+
 public class HawkerAdapter extends RecyclerView.Adapter<HawkerAdapter.HawkerViewHolder> {
 
     private Context context;
     private List<JSONObject> currentStockList;
     private Fragment fragment;
+
 
     public HawkerAdapter(Context context, List<JSONObject> currentStockList, Fragment fragment) {
         this.context = context;
@@ -35,13 +43,14 @@ public class HawkerAdapter extends RecyclerView.Adapter<HawkerAdapter.HawkerView
     @NonNull
     @Override
     public HawkerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
-        View view = LayoutInflater.from(context).inflate(R.layout.seller_cardview, viewGroup, false);
+        View view = LayoutInflater.from(context).inflate(layout.seller_cardview, viewGroup, false);
 
         return new HawkerViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HawkerViewHolder hawkerViewHolder, int i) {
+        setAnimation(hawkerViewHolder);
         JSONObject currentStock = currentStockList.get(i);
         hawkerViewHolder.setData(currentStock, i);
         String allVegName = "";
@@ -63,6 +72,14 @@ public class HawkerAdapter extends RecyclerView.Adapter<HawkerAdapter.HawkerView
         }
     }
 
+    private void setAnimation(HawkerViewHolder hawkerViewHolder) {
+        Animation slideImage = AnimationUtils.loadAnimation(context, anim.item_list_animation_logo);
+        hawkerViewHolder.sellerImageView.setAnimation(slideImage);
+
+        Animation slideRelativeLayout = AnimationUtils.loadAnimation(context,anim.item_list_animation_relativelayout);
+        hawkerViewHolder.container.setAnimation(slideRelativeLayout);
+    }
+
     @Override
     public int getItemCount() {
         return currentStockList.size();
@@ -76,13 +93,16 @@ public class HawkerAdapter extends RecyclerView.Adapter<HawkerAdapter.HawkerView
         TextView textSellerName, textVegName, textDescription;
         ImageView sellerImageView;
 
+        RelativeLayout container;
+
         public HawkerViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            textSellerName = itemView.findViewById(R.id.text_seller_name);
-            textVegName = itemView.findViewById(R.id.text_veg_name);
-            textDescription = itemView.findViewById(R.id.text_desc);
-            sellerImageView = itemView.findViewById(R.id.seller_imageview);
+            container=itemView.findViewById(id.container);
+            textSellerName = itemView.findViewById(id.text_seller_name);
+            textVegName = itemView.findViewById(id.text_veg_name);
+            textDescription = itemView.findViewById(id.text_desc);
+            sellerImageView = itemView.findViewById(id.seller_imageview);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -94,7 +114,7 @@ public class HawkerAdapter extends RecyclerView.Adapter<HawkerAdapter.HawkerView
                     ItemListFragment itemListFragment = new ItemListFragment();
                     itemListFragment.setArguments(bundle);
 
-                    fragment.getFragmentManager().beginTransaction().replace(R.id.home_fragment,
+                    fragment.getFragmentManager().beginTransaction().replace(id.home_fragment,
                             itemListFragment).addToBackStack(null).commit();
                 }
             });
